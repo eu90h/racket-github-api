8a3159df231be29838cd0da9d2ca7b8499dc65e8
Stefan <euler90h@gmail.com>
2015-08-09 03:33:41 -0400

    added procedures relating to blobs and commits

--- main.rkt
+++ main.rkt
@@ -303,4 +303,36 @@
 (define (github-delete-comment api-req repo-owner repo comment-id #:media-type [media-type ""])
   (api-req (string-append "/repos/" repo-owner "/" repo "/issues/comments/" comment-id)
            #:method "DELETE"))
-           
+
+(define (github-get-blob api-req repo-owner repo sha #:media-type [media-type ""])
+  (api-req (string-append "/repos/" repo-owner "/" repo "/git/blobs/" sha)
+           #:media-type media-type))
+
+(define (github-create-blob api-req repo-owner repo content [encoding "utf-8"] #:media-type [media-type ""])
+  (api-req (string-append "/repos/" repo-owner "/" repo "/git/blobs")
+           #:data (jsexpr->string
+                   (make-hash (list (cons 'content content)
+                                    (cons 'encoding encoding))))
+           #:method "POST"
+           #:media-type media-type))
+
+(define (github-get-commit api-req repo-owner repo sha #:media-type [media-type ""])
+  (api-string (string-append "/repos/" repo-owner "/" repo "/git/commits/" sha)
+              #:media-type media-type))
+
+(define (github-create-commit api-req repo-owner repo message tree parents
+                              #:media-type [media-type ""]
+                              #:author [author ""]
+                              #:email [author-email ""]
+                              #:data [data ""])
+  (let* ([commit-data (list (cons 'message message)
+                                (cons 'tree tree)
+                                (cons 'parents parents))]
+         [author-data (if (equal? author "") null (cons 'name author))]
+         [author-data (if (equal? author-email "") author-data (cons 'email email))]
+         [author-data (if (equal? date "") author-data (cons 'date date))])
+  (api-string (string-append "/repos/" repo-owner "/" repo "/git/commits")
+              (jsexpr->string (make-hash (if (null? author-data)
+                                             commit-data
+                                             (append commit-data
+                                                     (list (cons 'author author-data)))))))))
