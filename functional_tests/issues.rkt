b19c32769bb54574d8b608e88a9218e4ea379aff
Stefan <euler90h@gmail.com>
2015-08-10 03:27:15 -0400

    added more issues and issue comment tests

--- functional_tests/issues.rkt
+++ functional_tests/issues.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require json github-api "../../identity.rkt" rackunit "utils.rkt")
+(require json "../main.rkt" "../../identity.rkt" rackunit "utils.rkt")
 
 (define gh (github-api personal-token-id))
 
@@ -12,11 +12,11 @@
 (define user "eu90h")
 (define repo "api-test-repo")
 (define issue-body "this is a test of the issues api")
-
+(define issue-title "testing-issues-api")
 (define issue-data (github-create-issue gh
                                        user
                                        repo
-                                       "testing-issues-api"
+                                       issue-title
                                        #:body issue-body
                                        #:assignee user
                                        #:labels (list "woo!" "test")))
@@ -32,5 +32,34 @@
 (check-equal? (github-list-issue-comments gh user repo issue-number) null)
 (sub1-requests-remaining!)
 
+(check-equal? (hash-ref issue-data 'number)
+              (hash-ref (github-get-issue gh user repo issue-number) 'number))
+(sub1-requests-remaining!)
+
+(define comment-body "have a tissue")
+(define comment-data (github-create-comment gh
+                                            user
+                                            repo
+                                            issue-number
+                                            comment-body))
+(check-equal? (hash-ref comment-data 'body) comment-body)
+(sub1-requests-remaining!)
+
+(define comment-id (hash-ref comment-data 'id))
+
+(define new-comment-body "here's a thought: blah blah blah")
+(define new-comment-data (github-edit-comment gh user repo comment-id new-comment-body))
+(check-equal? (hash-ref new-comment-data 'body) new-comment-body)
+(sub1-requests-remaining!)
+
+(check-equal? (github-get-comment gh user repo comment-id) new-comment-data)
+(sub1-requests-remaining!)
+
+(check-status-code (github-delete-comment gh user repo comment-id) 204)
+(sub1-requests-remaining!)
+
 (check-equal? (hash-ref (github-edit-issue gh user repo issue-number #:state "closed") 'state) "closed")
+(sub1-requests-remaining!)
+
+(check-jsexpr? (github-list-issues gh user repo))
 (sub1-requests-remaining!)
